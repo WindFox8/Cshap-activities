57f53e078e4de040eceef4358f4520ce3fe85eec
Rafael Correr <119776059+WindFox8@users.noreply.github.com>
2024-03-20 13:01:51 -0300

    6º challenge commit

---
+++ 6/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Given a string s, return the longest palindromic substring in s.*/
+
+Console.Write("Digite a string a ser analisada: ");
+string s = Console.ReadLine();
+string palindromicSubstring = "";
+
+
+for (int i = 0; i < s.Length; i++){
+    for (int j = s.Length; j > i; j--){
+        string tempSubstring = s.Substring( i, j-i);
+        string tempSubstringReverse = string.Join("", tempSubstring.ToCharArray().Reverse());
+        if(tempSubstring == tempSubstringReverse){
+            if(tempSubstring.Length > palindromicSubstring.Length){
+                palindromicSubstring = tempSubstring;
+            }
+        }
+    }
+}
+
+Console.WriteLine($"A maior substring palindrômica na string {s} é {palindromicSubstring}");
